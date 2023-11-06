d5fb2940fa92e3c1efc3de73efede45e3c161158
SaileshBK <sailesh.bk1@icloud.com>
2023-11-05 23:21:40 -0600

    Chore(main): cleanup

--- Commands/MyCommand.cs
+++ Commands/MyCommand.cs
@@ -1,7 +1,7 @@
 ﻿using System.Globalization;
 using System.Linq;
 
-namespace Caseify
+namespace Caseify.Commands
 {
     [Command(PackageIds.MyCommand)]
     internal sealed class MyCommand : BaseCommand<MyCommand>
@@ -9,13 +9,13 @@ namespace Caseify
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
             var docView = await VS.Documents.GetActiveDocumentViewAsync().ConfigureAwait(false);
-            var selection = docView?.TextView.Selection.SelectedSpans.FirstOrDefault();
+            var selection = docView?.TextView?.Selection.SelectedSpans.FirstOrDefault();
 
             if (selection.HasValue)
             {
                 var text = selection.GetValueOrDefault();
-                var caseifiedText = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text.GetText().ToLower());
-                docView.TextView.TextBuffer.Replace(selection.Value, caseifiedText);
+                var transformedText = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text.GetText().ToLower());
+                docView.TextView.TextBuffer.Replace(selection.Value, transformedText);
             }
         }
     }
