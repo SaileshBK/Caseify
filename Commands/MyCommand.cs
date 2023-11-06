5abf4defaab6eedc2f77f19dc0a3efc32d62c44b
SaileshBK <sailesh.bk1@icloud.com>
2023-11-05 19:42:39 -0600

    Chore(main): Setup for MyCommand.cs

--- Commands/MyCommand.cs
+++ Commands/MyCommand.cs
@@ -1,11 +1,22 @@
-﻿namespace Caseify
+﻿using System.Globalization;
+using System.Linq;
+
+namespace Caseify
 {
     [Command(PackageIds.MyCommand)]
     internal sealed class MyCommand : BaseCommand<MyCommand>
     {
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
-            await VS.MessageBox.ShowWarningAsync("Caseify", "Button clicked");
+            var docView = await VS.Documents.GetActiveDocumentViewAsync().ConfigureAwait(false);
+            var selection = docView?.TextView.Selection.SelectedSpans.FirstOrDefault();
+
+            if (selection.HasValue)
+            {
+                var text = selection.GetValueOrDefault();
+                var caseifiedText = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text.GetText().ToLower());
+                docView.TextView.TextBuffer.Replace(selection.Value, caseifiedText);
+            }
         }
     }
 }
