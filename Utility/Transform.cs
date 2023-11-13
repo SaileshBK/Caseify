4dc4cb22cec1beb72f71c4f45cc69c5bbb30f5c1
SaileshBK <sailesh.bk1@icloud.com>
2023-11-12 22:10:54 -0600

    Chore(newMain): refactor some logic

---
+++ Utility/Transform.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace Caseify.Utility
+{
+    public static class Transform
+    {
+        public static char[] TextToChars(string selectedText)
+        {
+            var charText = selectedText.ToCharArray();
+            if (!charText.Any())
+            {
+                return null;
+            }
+
+            return charText;
+        }
+    }
+}
