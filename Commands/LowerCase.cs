ddf1eecd5cbc0e252b38eea86045acd2132213f7
SaileshBK <sailesh.bk1@icloud.com>
2023-11-13 21:57:37 -0600

    Feat(newMain): Added LowerCase.cs and changed icon

---
+++ Commands/LowerCase.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using Caseify.Utility;
+
+namespace Caseify.Commands;
+
+[Command(PackageIds.LowerCase)]
+internal sealed class LowerCase : BaseCommand<LowerCase>
+{
+    protected override async Task ExecuteAsync(OleMenuCmdEventArgs eventArgs)
+    {
+        await Transform.TextConverterAsync(CultureInfo.CurrentCulture.TextInfo.ToLower);
+    }
+}
