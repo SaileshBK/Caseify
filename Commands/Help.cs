59d7b151b0614a7689373fc462b42517bca584c9
SaileshBK <sailesh.bk1@icloud.com>
2023-11-24 00:26:20 -0600

    feat(newMain): Added new Help section for source code interaction

---
+++ Commands/Help.cs
@@ -0,0 +1,11 @@
+﻿namespace Caseify.Commands;
+
+[Command(PackageIds.Help)]
+internal sealed class Help : BaseCommand<Help>
+{
+    protected override Task ExecuteAsync(OleMenuCmdEventArgs e)
+    {
+        System.Diagnostics.Process.Start("https://github.com/SaileshBK/Caseify");
+        return Task.CompletedTask;
+    }
+}
