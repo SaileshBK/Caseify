715414e8f4335f3766494374ec7a3bfb68f5aeff
SaileshBK <sailesh.bk1@icloud.com>
2023-11-24 00:26:20 -0600

    feat(newMain): Added new Help section for source code interaction

--- VSCommandTable.cs
+++ VSCommandTable.cs
@@ -23,9 +23,11 @@ namespace Caseify
         public const int CaseifyMenu = 0x0001;
         public const int CaseifyListGroup = 0x0002;
         public const int CaseifySettings = 0x0003;
+        public const int CaseifyHelpSettings = 0x0004;
         public const int TitleCase = 0x0101;
         public const int CamelCase = 0x0102;
         public const int UpperCase = 0x0103;
         public const int LowerCase = 0x0104;
+        public const int Help = 0x0105;
     }
 }
